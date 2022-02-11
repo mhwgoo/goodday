9bbff8aef97c57dc4c0bf3ca8642a87e7b75ed8b
Kate Wang <kate.wang2018@gmail.com>
2022-02-11 15:23:12 +0800

    added stauts const

--- main.go
+++ main.go
@@ -14,55 +14,68 @@ type task struct {
 }
 
 type item struct {
-	name    string
-	checked bool
+	name   string
+	status statusID
 }
 
-func main() {
-	dt := time.Now()
-	fmt.Println(dt.Format("Mon Jan 02 15:04"))
-	hour := dt.Hour()
-	if hour >= 0 && hour <= 3 {
-		fmt.Printf("Good Night, %s :)", user)
-	} else if hour > 3 && hour < 12 {
-		fmt.Printf("Good Morning, %s :)", user)
-	} else if hour == 12 {
-		fmt.Printf("Good Noon, %s :)", user)
-	} else if hour > 12 && hour <= 15 {
-		fmt.Printf("Good Afternoon, %s :)", user)
-	} else if hour > 15 && hour < 20 {
-		fmt.Printf("Good Evening, %s :)", user)
-	} else if hour >= 20 && hour < 24 {
-		fmt.Printf("Good Night, %s :)", user)
-	}
+type statusID int
 
-	todos := task{
-		name:  "TO DO",
-		items: []item{{"speak: sentence", false}, {"go: godl", true}, {"go: achieve", false}},
+const (
+	STATUS_TODO statusID = iota
+	STATUS_DOING
+	STATUS_DONE
+)
+
+func main() {
+	codes := task{
+		name:  "TO CODE",
+		items: []item{{"go: godl", STATUS_DOING}, {"go: achieve", STATUS_DONE}, {"go: socket", STATUS_TODO}},
 	}
 
 	reads := task{
 		name:  "TO READ",
-		items: []item{{"The Apology", false}, {"The Guardian", false}},
+		items: []item{{"speak: sentence", STATUS_TODO}, {"The Apology", STATUS_DOING}, {"The Guardian", STATUS_TODO}},
 	}
 
 	problems := task{
 		name:  "TO SOLVE",
-		items: []item{{"virtualbox-guest-addons", false}, {"dual monitor", false}},
+		items: []item{{"virtualbox-guest-addons", STATUS_TODO}, {"dual monitor", STATUS_TODO}},
 	}
 
-	print(todos.name, todos.items)
+	print(codes.name, codes.items)
 	print(reads.name, reads.items)
 	print(problems.name, problems.items)
 }
 
 func print(title string, items []item) {
-	fmt.Printf("\n\n%s:\n", title)
+	fmt.Printf("\n%s:\n", title)
 	for key, value := range items {
-		if value.checked {
-			fmt.Printf("%d %s [x]\n", key+1, value.name)
-		} else {
+		switch value.status {
+		case 0:
 			fmt.Printf("%d %s [ ]\n", key+1, value.name)
+		case 1:
+			fmt.Printf("%d %s [=]\n", key+1, value.name)
+		case 2:
+			fmt.Printf("%d %s [x]\n", key+1, value.name)
 		}
+	}
+}
+
+func init() {
+	dt := time.Now()
+	fmt.Println(dt.Format("Mon Jan 02 15:04"))
+	hour := dt.Hour()
+	if hour >= 0 && hour <= 3 {
+		fmt.Printf("Good Night, %s :)\n", user)
+	} else if hour > 3 && hour < 12 {
+		fmt.Printf("Good Morning, %s :)\n", user)
+	} else if hour == 12 {
+		fmt.Printf("Good Noon, %s :)\n", user)
+	} else if hour > 12 && hour <= 15 {
+		fmt.Printf("Good Afternoon, %s :)\n", user)
+	} else if hour > 15 && hour < 20 {
+		fmt.Printf("Good Evening, %s :)\n", user)
+	} else if hour >= 20 && hour < 24 {
+		fmt.Printf("Good Night, %s :)\n", user)
 	}
 }
